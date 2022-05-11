441eb1ddfba07635523ea5d66cddf4d46c6d4b5b
samd82GB <samd82@yandex.ru>
2022-05-12 02:46:04 +0500

    Попытка прикрутить JavaFX, ошибки при запуске плагина javaFX на клиенте

---
+++ client/src/main/java/application/App.java
@@ -0,0 +1,25 @@
+package application;
+
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+public class App extends Application {
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/networkStorageMainScreen.fxml"));
+        primaryStage.show();
+        primaryStage.setScene(new Scene(root));
+
+    }
+}
